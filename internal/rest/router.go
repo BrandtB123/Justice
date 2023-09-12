f9b10aa6a82b3c07c70f5263abbe620d6b60326c
Brandt Rosentreter <brandtrosentreter@Brandts-Laptop.local>
2023-09-12 03:24:49 -0700

    dd

--- internal/rest/router.go
+++ internal/rest/router.go
@@ -7,10 +7,12 @@ import (
 func SetupRouter() *mux.Router {
 	r := mux.NewRouter()
 
+	// apis
 	r.HandleFunc("/", Base)
 	r.HandleFunc("/hello", Hello)
 	r.HandleFunc("/hello/{name}", Name)
 
+	// kube liveliness
 	r.HandleFunc("/health", Health)
 	r.HandleFunc("/readiness", Readiness)
 
